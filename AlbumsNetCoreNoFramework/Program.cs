3ee4d1a7ff98a122ac04bcca1246a8f0a1c09fdc
Jeff Schwartz <jeffschw@microsoft.com>
2022-10-11 07:52:39 -0700

    add rich responses

--- AlbumsNetCoreNoFramework/Program.cs
+++ AlbumsNetCoreNoFramework/Program.cs
@@ -7,7 +7,6 @@ var http = new HttpListener();
 
 var host = "127.0.0.1";
 var port = 5000;
-var contentType = "application/json";
 var concurrency = 10;
 
 http.Prefixes.Add($"http://{host}:{port}/");
@@ -33,50 +32,53 @@ async void ProcessRequestHandler(Task<HttpListenerContext> result)
     if (!http.IsListening) return;
     http.GetContextAsync().ContinueWith(ProcessRequestHandler);
 
-    var responseString = "";
+    Response response;
     if (context.Request.RawUrl.StartsWith("/albums", StringComparison.OrdinalIgnoreCase))
     {
         // POST
         if (context.Request.HttpMethod.Equals("POST",  StringComparison.OrdinalIgnoreCase))
         {
             var album = await JsonSerializer.DeserializeAsync<Album>(context.Request.InputStream, AlbumJsonSerializerContext.Default.Album);
-            responseString = PostAlbums(album);
+            response = PostAlbums(album);
         }
 
         // GET
         else
         {
             var idindx = context.Request.RawUrl.LastIndexOf('/');
-            if (idindx > 0 && idindx != context.Request.RawUrl.Length - 1) responseString = GetAlbumById(context.Request.RawUrl.Substring(idindx+1));
-            else responseString = GetAlbums();
+            if (idindx > 0 && idindx != context.Request.RawUrl.Length - 1) response = GetAlbumById(context.Request.RawUrl.Substring(idindx+1));
+            else response = GetAlbums();
         }
     }
-    else responseString = "not found";
+    else response = new Response("not found", 404);
 
     // write details
-    var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+    var buffer = System.Text.Encoding.UTF8.GetBytes(response.Content);
     context.Response.ContentLength64 = buffer.Length;
-    context.Response.ContentType = contentType;
+    context.Response.ContentType = response.Code == 200 ? "application/json" : "text/html";
+    context.Response.StatusCode = response.Code;
     using (var output = context.Response.OutputStream)
     {
         await output.WriteAsync(buffer, 0, buffer.Length);
     }
 }
 
+
+
 // GetAlbums responds with the list of all albums as JSON.
-string GetAlbums() => JsonSerializer.Serialize(myAlbums, typeof(Album[]), AlbumJsonSerializerContext.Default);
+Response GetAlbums() => new Response(JsonSerializer.Serialize(myAlbums, typeof(Album[]), AlbumJsonSerializerContext.Default), 200);
 
 // PostAlbums adds an album from JSON received in the request body.
-string PostAlbums(Album album)
+Response PostAlbums(Album album)
 {
     myAlbums = myAlbums.Append(album).ToArray();
-    return "success";
+    return new Response(JsonSerializer.Serialize(album, AlbumJsonSerializerContext.Default.Album), 200);
 }
 
 // GetAlbumById locates the album whose ID value matches the id parameter sent by the client, then returns that album as a response.
-string GetAlbumById(string id) => myAlbums.FirstOrDefault(a => a.Id == id) is { } album ? 
-                                           JsonSerializer.Serialize(album, AlbumJsonSerializerContext.Default.Album) : 
-                                           "album not found";
+Response GetAlbumById(string id) => myAlbums.FirstOrDefault(a => a.Id == id) is { } album ? 
+                                           new Response(JsonSerializer.Serialize(album, AlbumJsonSerializerContext.Default.Album), 200) : 
+                                           new Response("album not found", 404);
 
 [JsonSourceGenerationOptions(WriteIndented = true)]
 [JsonSerializable(typeof(Album))]
@@ -87,3 +89,5 @@ partial class AlbumJsonSerializerContext : JsonSerializerContext
 
 // Album represents data about a record album.
 internal record Album(string Id, string Title, string Artist, double Price);
+
+internal record Response(string Content, int Code);

342feb392c031622ac2757146c8553c5736b8f89
Eric Erhardt <eric.erhardt@microsoft.com>
2022-10-13 12:59:45 -0500

    Add Framework-Dependent apps

--- test/run-benchmarks.ps1
+++ test/run-benchmarks.ps1
@@ -11,13 +11,16 @@ function Run($commandLine)
 
 $commonArgs = "--config $benchmarkFile --profile aspnet-perf-lin --chart"
 
-Run "crank $commonArgs --scenario albums-netcore-alpine --json $testResultsDir\net-alpine-serverGC.json --property platform=netcore-alpine-serverGC --application.buildArguments SERVER_GC=true"
-Run "crank $commonArgs --scenario albums-netcore-alpine --json $testResultsDir\net-alpine-workstationGC.json --property platform=netcore-alpine-workstationGC --application.buildArguments SERVER_GC=false"
+Run "crank $commonArgs --scenario albums-net-alpine --json $testResultsDir\net-alpine-serverGC.json --property platform=net-alpine-serverGC --application.buildArguments SERVER_GC=true"
+Run "crank $commonArgs --scenario albums-net-alpine --json $testResultsDir\net-alpine-workstationGC.json --property platform=net-alpine-workstationGC --application.buildArguments SERVER_GC=false"
+
+Run "crank $commonArgs --scenario albums-net-fdd --json $testResultsDir\net-fdd-serverGC.json --property platform=net-fdd-serverGC --application.buildArguments SERVER_GC=true"
+Run "crank $commonArgs --scenario albums-net-fdd --json $testResultsDir\net-fdd-workstationGC.json --property platform=net-fdd-workstationGC --application.buildArguments SERVER_GC=false"
 
 Run "crank $commonArgs --scenario albums-httplistener-alpine --json $testResultsDir\httplistener-alpine.json --property platform=httplistener-alpine"
 
-Run "crank $commonArgs --scenario albums-netcore-debian --json $testResultsDir\net-debian.json --property platform=netcore-debian"
+Run "crank $commonArgs --scenario albums-net-debian --json $testResultsDir\net-debian.json --property platform=net-debian"
 
 Run "crank $commonArgs --scenario albums-go-alpine --json $testResultsDir\go-alpine.json --property platform=go-alpine"
 
-Run "crank compare $testResultsDir\go-alpine.json $testResultsDir\net-alpine-serverGC.json $testResultsDir\net-alpine-workstationGC.json $testResultsDir\net-debian.json $testResultsDir\httplistener-alpine.json"
+Run "crank compare $testResultsDir\go-alpine.json $testResultsDir\net-alpine-serverGC.json $testResultsDir\net-alpine-workstationGC.json $testResultsDir\net-fdd-serverGC.json $testResultsDir\net-fdd-workstationGC.json $testResultsDir\net-debian.json $testResultsDir\httplistener-alpine.json"
